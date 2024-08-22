f8e5cf4a16450d03630a3f55e43bdd46e36d4a79
houndhollis <houndhollis9797@gmail.com>
2024-08-22 19:02:35 +0900

    [⚒️ CREATE] 메인 레이아웃 추가

---
+++ layouts/MainLayout.tsx
@@ -0,0 +1,10 @@
+export default function MainLayout({ children }) {
+  return (
+    <main
+      style={{ boxShadow: "0px 0px 16px rgb(50 50 50 / 12%)" }}
+      className="w-full h-screen bg-white shadow-2xl"
+    >
+      {children}
+    </main>
+  );
+}

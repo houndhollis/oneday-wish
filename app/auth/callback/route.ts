9c22dbd8b41bee4643b1ab1f431124e8247ff43a
houndhollis <houndhollis9797@gmail.com>
2024-08-23 19:43:53 +0900

    [⭐️ FEAT] 로그인 API 추가

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { createServerSupabaseClient } from "utils/supabase/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/home";
+
+  if (code) {
+    const supabase = await createServerSupabaseClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}

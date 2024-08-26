019bf1c364c97d1788811e74bea3c5f044959955
houndhollis <houndhollis9797@gmail.com>
2024-08-27 00:03:44 +0900

    [⭐️ FEAT] 글 작성 기능 추가

--- components/Post/PostSection/index.tsx
+++ components/Post/PostSection/index.tsx
@@ -1,11 +1,52 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+import { createPost, uploadFile } from "actions/storageActions";
 import FormControl from "components/FormControl";
 import UploadImage from "components/UploadImage";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { imageState } from "utils/recoil/atoms";
+import { getImageUrl } from "utils/supabase/storage";
+import { useRouter } from "next/navigation";
 
-export default function PostSection() {
+export default function PostSection({ session }) {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const imageObjectState = useRecoilValue(imageState);
+
+  const postMutation = useMutation({
+    mutationFn: async (image) => {
+      createPost({
+        author: session?.user?.user_metadata?.email,
+        title,
+        content,
+        image_url: getImageUrl(image),
+        profile_image: session?.user?.user_metadata?.avatar_url,
+      });
+    },
+    onSuccess: () => {
+      console.log("성공");
+      router.replace("/home");
+    },
+  });
+
+  const handleSubmit = async () => {
+    if (!title || !content) {
+      return alert("제목 또는 내용을 입력해주세요.");
+    }
+
+    let fileResult;
+    if (imageObjectState) {
+      console.log(imageObjectState.get("file"));
+      fileResult = await uploadFile(imageObjectState);
+    }
+    postMutation.mutate(fileResult?.path);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col">
       <UploadImage />
       <FormControl
         className="mt-4 gap-2"
@@ -16,6 +57,8 @@ export default function PostSection() {
           className="py-2 px-1 border-b border-gray-200 focus:outline-none"
           id="title"
           placeholder="제목을 입력해주세요"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </FormControl>
       <FormControl className="mt-6" htmlFor="content">
@@ -23,8 +66,16 @@ export default function PostSection() {
           className="h-[300px] py-2 px-1 border rounded border-gray-200 focus:outline-none resize-none"
           id="content"
           placeholder="내용을 입력해주세요"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
       </FormControl>
+      <button
+        onClick={handleSubmit}
+        className="self-end mt-3 px-3 py-1.5 flex items-center justify-center bg-black text-white rounded-[8px]"
+      >
+        작성하기
+      </button>
     </div>
   );
 }

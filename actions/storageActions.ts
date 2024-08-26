ec6b78963964f26c12ce0083601bdab92cf76ad9
houndhollis <houndhollis9797@gmail.com>
2024-08-26 22:01:39 +0900

    [✅ FIX] 슈파베이스 스토리지 한글 파일 업로드 안되는 문제

--- actions/storageActions.ts
+++ actions/storageActions.ts
@@ -12,12 +12,46 @@ function handleError(error) {
 export async function uploadFile(formData) {
   const supabase = await createServerSupabaseClient();
   const file = formData.get("file") as File;
+  const safeFileName = file.name
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036F]/g, "")
+    .replace(/[^a-zA-Z0-9.-]/g, "_");
 
   const { data, error } = await supabase.storage
     .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET)
-    .upload(file.name, file, { upsert: true });
+    .upload(safeFileName, file, { upsert: true });
 
   handleError(error);
+
+  return data;
+}
+
+export async function getPosts() {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("oneday")
+    .select("*")
+    .order("created_at", { ascending: true });
+
+  if (error) {
+    handleError(error);
+  }
+
+  return data;
+}
+
+export async function createPost(post) {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase.from("oneday").insert({
+    ...post,
+    created_at: new Date().toISOString(),
+  });
+
+  if (error) {
+    handleError(error);
+  }
 
   return data;
 }

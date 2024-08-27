e685c981c5254fb32114b48a2fda60738bc7595a
houndhollis <houndhollis9797@gmail.com>
2024-08-27 15:00:53 +0900

    [⭐️ FEAT] 좋아요 기능 슈파베이스 기능 추가

---
+++ actions/postActions.ts
@@ -0,0 +1,143 @@
+"use server";
+
+import { createServerSupabaseClient } from "utils/supabase/server";
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getPosts(userId) {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("oneday")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    handleError(error);
+    return [];
+  }
+
+  if (userId) {
+    const postIds = data.map((post) => post.id);
+    const { data: likesData, error: likesError } = await supabase
+      .from("likes")
+      .select("post_id")
+      .eq("user_id", userId)
+      .in("post_id", postIds);
+
+    if (likesError) {
+      handleError(likesError);
+      return [];
+    }
+
+    const likedPosts = likesData.map((like) => like.post_id);
+
+    return data.map((post) => ({
+      ...post,
+      liked_by_user: likedPosts.includes(post.id),
+    }));
+  } else {
+    return data.map((post) => ({
+      ...post,
+      liked_by_user: false,
+    }));
+  }
+}
+
+export async function createPost(post) {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase.from("oneday").insert({
+    ...post,
+    created_at: new Date().toISOString(),
+  });
+
+  if (error) {
+    handleError(error);
+  }
+
+  return data;
+}
+
+export async function toggleLike(userId, postId, currentLikeStatus) {
+  const supabase = await createServerSupabaseClient();
+
+  if (userId === null || userId === undefined) {
+    throw new Error("User not authenticated");
+  }
+
+  if (currentLikeStatus) {
+    const { error } = await supabase
+      .from("likes")
+      .delete()
+      .eq("user_id", userId)
+      .eq("post_id", postId);
+
+    if (error) {
+      handleError(error);
+      return false;
+    }
+    // likes 감소 쿼리
+    const { data: postData, error: postError } = await supabase
+      .from("oneday")
+      .select("likes_count")
+      .eq("id", postId)
+      .single();
+
+    if (postError) {
+      handleError(postError);
+      return false;
+    }
+
+    const currentLikesCount = postData.likes_count;
+
+    const { error: decrementError } = await supabase
+      .from("oneday")
+      .update({ likes_count: currentLikesCount - 1 })
+      .eq("id", postId);
+
+    if (decrementError) {
+      handleError(decrementError);
+      return false;
+    }
+  } else {
+    const { error } = await supabase
+      .from("likes")
+      .insert([{ user_id: userId, post_id: postId }]);
+
+    if (error) {
+      handleError(error);
+      return false;
+    }
+    // likes_count를 증가시키는 쿼리
+    const { data: postData, error: postError } = await supabase
+      .from("oneday")
+      .select("likes_count")
+      .eq("id", postId)
+      .single();
+
+    if (postError) {
+      handleError(postError);
+      return false;
+    }
+
+    const currentLikesCount = postData.likes_count;
+
+    const { error: incrementError } = await supabase
+      .from("oneday")
+      .update({ likes_count: currentLikesCount + 1 })
+      .eq("id", postId);
+
+    if (incrementError) {
+      handleError(incrementError);
+      return false;
+    }
+  }
+
+  return true;
+}

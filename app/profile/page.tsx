fe9125d430191dcec22a2cbca3674d1b863f6bca
houndhollis <houndhollis9797@gmail.com>
2024-08-28 00:34:42 +0900

    [⭐️ FEAT] 프로필 페이지 추가

---
+++ app/profile/page.tsx
@@ -0,0 +1,18 @@
+import Header from "components/Header";
+import ProfileSection from "components/Profile";
+import { createServerSupabaseClient } from "utils/supabase/server";
+
+export default async function Profile() {
+  const supabase = await createServerSupabaseClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <div>
+      <Header session={session} />
+      <ProfileSection session={session} />
+    </div>
+  );
+}

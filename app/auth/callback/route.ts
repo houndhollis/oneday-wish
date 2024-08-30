fb5eb3016c4f1cb77c90b65fc97fd02a7d05486e
houndhollis <houndhollis9797@gmail.com>
2024-08-30 16:27:56 +0900

    [🔫 CHORE] 코드 원복

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,20 +3,15 @@ import { createServerSupabaseClient } from "utils/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  console.log(origin, request.url);
+
   const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/home";
 
   if (code) {
     const supabase = await createServerSupabaseClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(
-        `${
-          process.env.NEXT_PUBLIC_VERCEL_URL
-            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/home`
-            : "http://localhost:3000/home"
-        }`
-      );
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 

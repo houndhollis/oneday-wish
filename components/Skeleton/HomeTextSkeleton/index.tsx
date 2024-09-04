3c6bd4485a7e72e0bdc3f2dad9338c4ba883a791
houndhollis <houndhollis9797@gmail.com>
2024-09-04 20:48:13 +0900

    [⭐️ FEAT] text list 스켈레톤 추가

--- components/Skeleton/HomeTextSkeleton/index.tsx
+++ components/Skeleton/HomeTextSkeleton/index.tsx
@@ -1,6 +1,31 @@
-export default function HomeTextSkeleton() {
+export default function HomeTextSkeleton({ isHome = true }) {
+  if (!isHome) {
+    return (
+      <div className="mt-6 flex flex-col gap-10 animate-pulse h-[94px]">
+        {Array.from({ length: 8 }).map((_) => (
+          <HomeTextListItem />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="mt-1.5 p-4 animate-pulse h-[86px]">
+      <div className="flex justify-between items-center">
+        <div className="w-1/4 h-2 bg-slate-200" />
+        <div className="w-1/6 h-2 bg-slate-200" />
+      </div>
+      <div className="mt-6 flex flex-col gap-3">
+        <div className="w-1/2 h-4 bg-slate-200" />
+        <div className="w-1/4 h-2 bg-slate-200" />
+      </div>
+    </div>
+  );
+}
+
+export function HomeTextListItem() {
+  return (
+    <div>
       <div className="flex justify-between items-center">
         <div className="w-1/4 h-2 bg-slate-200" />
         <div className="w-1/6 h-2 bg-slate-200" />

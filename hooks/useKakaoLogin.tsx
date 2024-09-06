a68a1010b4cf7f41e68bab3028fbd12a2940ae7f
houndhollis <houndhollis9797@gmail.com>
2024-09-06 21:24:43 +0900

    [🎉 REFACTOR] 카카오 로그인 hook 변경

--- hooks/useKakaoLogin.tsx
+++ hooks/useKakaoLogin.tsx
@@ -1,19 +1,37 @@
 "use client";
+
+import { useCallback, useState } from "react";
 import { createBrowserSupabaseClient } from "utils/supabase/client";
 
-export const useKaKaoLogin = async () => {
-  const supabase = await createBrowserSupabaseClient();
+export const useKaKaoLogin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const supabase = createBrowserSupabaseClient();
 
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "kakao",
-    options: {
-      redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `https://daily-connection.vercel.app//auth/callback`
-        : "http://localhost:3000/auth/callback",
-    },
-  });
+  const loginAuthKakao = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "kakao",
+        options: {
+          redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://daily-connection.vercel.app/auth/callback`
+            : "http://localhost:3000/auth/callback",
+        },
+      });
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      setError(error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [supabase]);
 
   if (error) {
     alert(error.message);
   }
+  return { loginAuthKakao, isLoading, error };
 };

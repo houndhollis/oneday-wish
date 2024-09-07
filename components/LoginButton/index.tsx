af8999d9fa12344c03d6347448dee6391e5a6e87
houndhollis <houndhollis9797@gmail.com>
2024-09-07 19:59:46 +0900

    [✏️ STYLE] 커스텀 if 문 위치 변경

--- components/LoginButton/index.tsx
+++ components/LoginButton/index.tsx
@@ -14,30 +14,31 @@ export default function LoginButton({ isCustom = false }: LoginButtonProp) {
 
   if (isCustom) {
     return (
-      <button onClick={loginAuthKakao} disabled={isLoading} className="mt-5">
-        {isLoading ? (
-          <div className="text-[14px] w-[300px] h-[45px] bg-[#fee601] rounded-[8px] flex items-center justify-center">
-            로그인중...
-          </div>
-        ) : (
-          <Image
-            src={KaKaoLoginButton}
-            alt="카카오 로그인 버튼"
-            width={300}
-            height={45}
-          />
-        )}
+      <button
+        onClick={loginAuthKakao}
+        disabled={isLoading}
+        className="bg-black text-white rounded flex items-center justify-center text-[12px] w-[48px] h-[32px] px-2 "
+      >
+        {isLoading ? <ClipLoader size={16} color="white" /> : "로그인"}
+        {error && <p className="text-[14px] text-red-500">{error}</p>}
       </button>
     );
   }
 
   return (
-    <button
-      onClick={loginAuthKakao}
-      disabled={isLoading}
-      className="bg-black text-white rounded flex items-center justify-center text-[12px] w-[48px] h-[32px] px-2 "
-    >
-      {isLoading ? <ClipLoader size={16} color="white" /> : "로그인"}
+    <button onClick={loginAuthKakao} disabled={isLoading} className="mt-5">
+      {isLoading ? (
+        <div className="text-[14px] w-[300px] h-[45px] bg-[#fee601] rounded-[8px] flex items-center justify-center">
+          로그인중...
+        </div>
+      ) : (
+        <Image
+          src={KaKaoLoginButton}
+          alt="카카오 로그인 버튼"
+          width={300}
+          height={45}
+        />
+      )}
     </button>
   );
 }
